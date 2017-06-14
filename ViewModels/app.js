e5f2e08ba7b018644d12f030f5ea76deb65e9b9a
Victor <victorarokiyarajh@fss.co.in>
2017-06-14 19:33:22 +0530

    Create app.js

--- ViewModels/app.js
+++ ViewModels/app.js
@@ -60,3 +60,41 @@ io.on('connection', function (socket) {
   });
 
 });
+
+
+
+{
+  "name": "fetchReports",
+  "version": "1.0.0",
+  "description": "Pull report based on game start and end date",
+  "main": "server.js",
+  "scripts": {
+    "test": "./node_modules/.bin/jasmine-node spec",
+    "start": "node server.js"
+  },
+  "author": "Victor",
+  "license": "ISC",
+  "dependencies": {
+    "angular": "^1.6.4",
+    "body-parser": "^1.15.2",
+    "config": "^1.24.0",
+    "cookie-parser": "^1.4.3",
+    "cors": "^2.8.0",
+    "csv-parse": "^1.2.0",
+    "dateformat": "^2.0.0",
+    "express": "^4.15.3",
+    "forever": "^0.15.3",
+    "fs": "0.0.1-security",
+    "fs-extra": "^0.30.0",
+    "http": "0.0.0",
+    "jasmine-node": "^1.14.5",
+    "mongodb": "^2.2.28",
+    "node-cron": "^1.1.3",
+    "nodemailer": "^3.1.5",
+    "request": "^2.81.0",
+    "socket.io": "^2.0.3",
+    "underscore": "^1.8.3",
+    "winston": "^2.3.0",
+    "winston-daily-rotate-file": "^1.3.1"
+  }
+}
