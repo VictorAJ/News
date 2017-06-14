7ab39c0fa3f6b0560e6087e373905b74999f6c97
Victor <victorarokiyarajh@fss.co.in>
2017-06-14 19:33:00 +0530

    Update app.js

--- ViewModels/app.js
+++ ViewModels/app.js
@@ -19,3 +19,44 @@ app.config(function($routeProvider, $mdIconProvider) {
         templateUrl: "Templates/home.html"
     });;
 })
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var MongoClient = require('mongodb').MongoClient;
+var socketObj;
+var dbObj;
+server.listen(9903);
+
+app.get('/', function (req, res) {
+    // if(dbObj != undefined) {            
+    //     var collection = dbObj.collection('NewChartCollection');
+    //     var lotsOfDocs = [
+    //                 {name: 'www.site1.com', upload: random(), download: random(), total: random()},
+    //                 {name: 'www.site2.com', upload: random(), download: random(), total: random()},
+    //                 {name: 'www.site3.com', upload: random(), download: random(), total: random()},
+    //                 {name: 'www.site4.com', upload: random(), download: random(), total: random()},
+    //         ];
+
+    //     collection.insert(lotsOfDocs, {w:1}, function(err, result) {
+    //         collection.find({}, { _id: 0}).toArray(function(err, items) {
+    //             console.log(items);
+    //             socketObj.emit('news', items );
+    //         });
+    //     });
+    // }
+  res.send('ok');
+});
+
+io.on('connection', function (socket) {
+    MongoClient.connect("mongodb://victor:Admin123@ds127132.mlab.com:27132/passmanager", function(err, db) {
+        if(!err) {
+            console.log("We are connected");
+            dbObj =  db;
+        }
+    });
+  socketObj = socket;
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+});
